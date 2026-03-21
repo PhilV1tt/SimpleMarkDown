b076df01bcdaa05ac8e8ef38b9148b4000d21c79
PhilV1tt <philemon.vittet@gmail.com>
2026-03-21 10:13:31 +0100

    Initial Commit

---
+++ SimpleMarkDown/SimpleMarkDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMarkDownApp.swift
+//  SimpleMarkDown
+//
+//  Created by vitt on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMarkDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
